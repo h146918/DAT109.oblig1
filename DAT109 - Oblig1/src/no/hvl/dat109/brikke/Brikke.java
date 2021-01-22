19d5b703baa90e7e3ef93fce202ea391d3317078
cerberus <cerberus@DESKTOP-LUJ2O9O.lan>
2021-01-22 16:45:45 +0100

    Oppdatert Brikke og Ruteklasse. Ny sjekk av oppdatertbrikke metode med ny beregning med spesefikke tilfeller

--- DAT109 - Oblig1/src/no/hvl/dat109/brikke/Brikke.java
+++ DAT109 - Oblig1/src/no/hvl/dat109/brikke/Brikke.java
@@ -1,44 +1,59 @@
 package no.hvl.dat109.brikke;
 
+import no.hvl.dat109.rute.Rute;
+
 public class Brikke {
-	
+
 	private String navn;
-	private int rute;
-	
-	
+	private Rute rute;
+
 	public Brikke(String navn) {
-		
+
 		this.navn = navn;
-		this.rute = 1;
-		
+
 	}
 
-	
 	public void oppdaterRute(int terning) {
-		this.rute = this.rute + terning;
+
+		switch (rute.getRutenr()) {
+		case 37:
+			rute.setRutenr(20);
+			break;
+		case 70:
+			rute.setRutenr(32);
+			break;
+		case 99:
+			rute.setRutenr(64);
+			break;
+		case 27:
+			rute.setRutenr(46);
+			break;
+		case 42:
+			rute.setRutenr(62);
+			break;
+		case 66:
+			rute.setRutenr(88);
+			break;
+		default:
+			rute.setRutenr(rute.getRutenr() + terning);
+		}
+
 	}
-	
 
 	public String getNavn() {
 		return navn;
 	}
 
-
 	public void setNavn(String navn) {
 		this.navn = navn;
 	}
 
-
-	public int getRute() {
+	public Rute getRute() {
 		return rute;
 	}
 
-
-	public void setRute(int rute) {
+	public void setRute(Rute rute) {
 		this.rute = rute;
 	}
-	
-	
-	
 
 }
--- DAT109 - Oblig1/src/no/hvl/dat109/rute/Rute.java
+++ DAT109 - Oblig1/src/no/hvl/dat109/rute/Rute.java
@@ -1,12 +1,21 @@
 package no.hvl.dat109.rute;
 
 public class Rute {
-	
-	public Rute () {
-		
-		
-		
-		
+
+	private int rutenr;
+
+	public Rute(int rutenr) {
+
+		this.rutenr = rutenr;
+
+	}
+
+	public int getRutenr() {
+		return rutenr;
+	}
+
+	public void setRutenr(int rutenr) {
+		this.rutenr = rutenr;
 	}
 
 }
